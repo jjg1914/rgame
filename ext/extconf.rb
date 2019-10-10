0a8ad7738959357a750bcebf4c07143d78c0a2fd
John J. Glynn IV <jjg1914@rit.edu>
2019-10-09 21:19:46 -0400

    build fix

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,6 @@
 require "mkmf"
 
-find_header("SDL.h", "/usr/local/include/SDL2") or abort "Can't find the SDL.h header"
+find_header("SDL.h", "/usr/local/include/SDL2", "/usr/include/SDL2") or abort "Can't find the SDL.h header"
 find_library("SDL2", "SDL_Init") or abort ("Can't find the SDL library")
 find_library("SDL2_ttf", "TTF_Init") or abort ("Can't find the SDL_ttf library")
 
